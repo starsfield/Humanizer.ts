9063983da188e525932c47ae5b5119396e17cabc
starsfield <111563774+starsfield@users.noreply.github.com>
2022-05-28 20:27:14 +0000

    0.3.8

--- Main_Menu.py
+++ Main_Menu.py
@@ -72,4 +72,4 @@ def mainMenu (window):
             helpScreen(win, menuObjects)
             mainMenu(win)
             break
-        if click.getX() > 30 and cli
+        if click.getX() > 30 and click.ge
