ee2a9112e68e09c0468d9af973d8187c946114d4
starsfield <111563774+starsfield@users.noreply.github.com>
2022-05-30 18:23:12 +0000

    DEV-1416: refactor

--- Main_Menu.py
+++ Main_Menu.py
@@ -72,4 +72,4 @@ def mainMenu (window):
             helpScreen(win, menuObjects)
             mainMenu(win)
             break
-        if click.getX() > 30 and click.getX() < 70 and click.
+        if click.getX() > 30 and click.getX() < 70 and click.getY()
