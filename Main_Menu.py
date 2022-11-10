f61545dc1ad6c44738d76ebc30a991107bc03228
starsfield <111563774+starsfield@users.noreply.github.com>
2022-11-10 14:21:02 +0000

    Add content to README.md

--- Main_Menu.py
+++ Main_Menu.py
@@ -91,4 +91,4 @@ def helpScreen(win, menuObjects):
     helpObjects.append(exitHelpBox)
     exitHelpText = Text(Point(50, 5), "Click anywhere to go back to the Main Menu")
     exitHelpText.setSize(10)
-    helpObjects.append(exitHel
+    helpObjects.append(exitHelpText
